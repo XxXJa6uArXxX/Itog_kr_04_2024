05c13a74cdc262554eaf0ea139fe9e674b1cc26e
XxXJa6uArXxX <valera5xxxjaguarxxx5@mail.ru>
2024-04-10 00:27:59 +0500

    Исправил все ошибки в написании программы и довел её до стадии возможности выполнения в консоли. На данный момент мы можем создать массив из Нного количества строк и заполнить его теми строками, которые сами введем с клавиатуры

--- Program.cs
+++ Program.cs
@@ -2,17 +2,21 @@
 
 class Prog
 {
+    static void Main()
+   
+ {
 
 Console.WriteLine("Введите количество строк в массиве:");
 int N = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[N];
 
-for (int i = 0, i < N, int++)
+for (int i = 0; i < N; i++)
 
  {
-            Console.WriteLine("Введите элементы для массива №{i}:");
-            initialArray[i] = Console.ReadLine();
+            Console.WriteLine($"Введите элементы для {i+1} строки:");
+            arr[i] = Console.ReadLine();
  }
 
-string[] arr = new string[N];
+}
 
 }
--- obj/Debug/net8.0/Itog_kr_04_2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Itog_kr_04_2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Itog_kr_04_2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c75a1bad4d4f2b5f81d0bbec261a52233ba82c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511fbe8f973f22b5ba6b258cf1be6898f52dae2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Itog_kr_04_2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Itog_kr_04_2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
