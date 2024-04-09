511fbe8f973f22b5ba6b258cf1be6898f52dae2b
XxXJa6uArXxX <valera5xxxjaguarxxx5@mail.ru>
2024-04-10 00:15:28 +0500

    Добавил файл 'git.ignore' в репозиторий и начал написание программы: часть, где задаются элементы массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Prog
+{
+
+Console.WriteLine("Введите количество строк в массиве:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0, i < N, int++)
+
+ {
+            Console.WriteLine("Введите элементы для массива №{i}:");
+            initialArray[i] = Console.ReadLine();
+ }
+
+string[] arr = new string[N];
+
+}
